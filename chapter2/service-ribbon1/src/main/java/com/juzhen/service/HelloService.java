d419838877a298a0789e29499ed63ab5e7d755a4
jackchow <jkzzit@163.com>
2017-12-04 19:50:49 +0800

    ribbon+restTemplate的服务调用方式

--- chapter2/service-ribbon1/src/main/java/com/juzhen/service/HelloService.java
+++ chapter2/service-ribbon1/src/main/java/com/juzhen/service/HelloService.java
@@ -15,7 +15,7 @@ public class HelloService {
     RestTemplate restTemplate;
 
     public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+        return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+name,String.class);
     }
 
 }
