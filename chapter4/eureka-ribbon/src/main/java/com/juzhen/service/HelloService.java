4a0fc88e425b86f0512d2d82802fcfcc97019340
jackchow <jkzzit@163.com>
2017-12-09 01:19:01 +0800

    Hystrix组件，实现了在ribbon使用断路器

--- chapter4/eureka-ribbon/src/main/java/com/juzhen/service/HelloService.java
+++ chapter4/eureka-ribbon/src/main/java/com/juzhen/service/HelloService.java
@@ -14,7 +14,7 @@ public class HelloService {
 
     @Autowired
     RestTemplate restTemplate;
-
+    //使用在ribbon使用断路器
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+name,String.class);
