a9e042a712213f87f432e9fbd779811acf718b92
jackchow <jkzzit@163.com>
2017-12-09 02:18:48 +0800

    Hystrix组件，实现ribbon中使用断路器(断路器：Hystrix 仪表盘)

--- chapter4/eureka-ribbon/src/main/java/com/juzhen/EurekaRibbonApplication.java
+++ chapter4/eureka-ribbon/src/main/java/com/juzhen/EurekaRibbonApplication.java
@@ -2,15 +2,19 @@ package com.juzhen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @EnableHystrix  //加@EnableHystrix注解开启Hystrix
+@EnableHystrixDashboard //加注解，开启hystrixDashboard
 public class EurekaRibbonApplication {
 
 	public static void main(String[] args) {
