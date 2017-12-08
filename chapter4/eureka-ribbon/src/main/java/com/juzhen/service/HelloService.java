5476719eb4df0782680bc05b9bbc27f1a8c17830
jackchow <jkzzit@163.com>
2017-12-09 01:50:37 +0800

    Hystrix组件，实现Feign中使用断路器

--- chapter4/eureka-ribbon/src/main/java/com/juzhen/service/HelloService.java
+++ chapter4/eureka-ribbon/src/main/java/com/juzhen/service/HelloService.java
@@ -14,7 +14,8 @@ public class HelloService {
 
     @Autowired
     RestTemplate restTemplate;
-    //使用在ribbon使用断路器
+    //使用在ribbon使用断路器，当时eureka-client工程不可用时，eureka-ribbon 调用eureka-client的API接口时会执行
+    //执行快速失败，直接返回一组字符串，而不是等待响应超时，这很好地控制了容器的线程阻塞；
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+name,String.class);
--- chapter4/serice-feign/src/main/java/com/juzhen/service/SchedualServiceHi.java
+++ chapter4/serice-feign/src/main/java/com/juzhen/service/SchedualServiceHi.java
@@ -1,11 +1,12 @@
 package com.juzhen.service;
 
+import com.juzhen.service.impl.SchedualServiceHiHystric;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eureka-client")//定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务
+@FeignClient(value = "eureka-client",fallback = SchedualServiceHiHystric.class)//定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
