e37e4e89579337fad913d887419bcd385916fc05
jackchow <jkzzit@163.com>
2017-12-09 01:16:50 +0800

    spring cloudspring cloud

---
+++ chapter4/serice-feign/src/main/java/com/juzhen/service/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.juzhen.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "eureka-client")//定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+}
