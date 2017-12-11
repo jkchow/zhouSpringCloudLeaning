f1e514a0c771f846a80b47332f9e52b5754081f2
jackchow <jkzzit@163.com>
2017-12-11 18:56:12 +0800

    spring cloud

--- chapter5/service-ribbon/src/main/java/com/forezp/web/HelloControler.java
+++ chapter5/service-ribbon/src/main/java/com/forezp/web/HelloControler.java
@@ -2,6 +2,7 @@ package com.forezp.web;
 
 import com.forezp.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloControler {
     @Autowired
     HelloService helloService;
+
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping(value = "/hi")
     public String hi(@RequestParam String name){
         return helloService.hiService(name);
     }
-
+    @RequestMapping("/hii")
+    public String home(@RequestParam String name) {
+        return "hi12 "+name+",i am from port:" +port;
+    }
 
 }
