01e05e717c637111aca865196da91ffa47a71d64
jackchow <jkzzit@163.com>
2017-12-12 21:08:39 +0800

    spring cloud bus

---
+++ chapter8/config-client/src/main/java/com/juzhen/ConfigClientApplication.java
@@ -0,0 +1,28 @@
+package com.juzhen;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@RefreshScope //加在有注入从configServer取得的参数的类，我猜含义是重新注入使用 post http://localhost:8881/bus/refresh
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+}
